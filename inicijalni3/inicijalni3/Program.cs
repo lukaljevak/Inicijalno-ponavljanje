3c8c5fdf56e2a7b9e5a445bdc301766ba192d0b8
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-09-11 09:07:07 +0200

    Add files via upload

---
+++ inicijalni3/inicijalni3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalni3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+
+            double prosjek = (a + b + c + d) / 4;
+
+            Console.WriteLine("Prosjek vrijednosti je: " + prosjek);
+            Console.ReadKey();
+        }
+    }
+}
