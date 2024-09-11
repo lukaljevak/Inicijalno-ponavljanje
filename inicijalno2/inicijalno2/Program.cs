9c4244417899deef9f1bfe04c63977785ea5a8c2
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-09-11 08:56:45 +0200

    Add files via upload

---
+++ inicijalno2/inicijalno2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inicijalno2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string niz = Console.ReadLine();
+            string rezultat = niz.Replace(' ', '_');
+            Console.WriteLine("Rezultat: " + rezultat);
+            Console.ReadKey();
+        }
+    }
+}
